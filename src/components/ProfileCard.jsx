56a1d8dc5f14472c445863258cf7f812a792a3c4
Ankur Sarkar <sarkarrohonankur@gmail.com>
2024-05-18 21:46:45 +0530

    update css

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -1,82 +1,86 @@
-import React from 'react'
-import "@/styles/profilecard.css"
+"use client"
+import React, { useState } from 'react';
+import { FaEnvelope, FaFacebook, FaInstagram, FaGooglePlusG, FaTwitter, FaCamera } from 'react-icons/fa';
+import "@/styles/profilecard.css";
+
 const ProfileCard = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isHoverActive, setIsHoverActive] = useState(false);
+
+  const show = () => {
+    setIsHoverActive(true);
+    setIsModalVisible(true);
+  };
+
+  const hide = () => {
+    setIsHoverActive(false);
+    setIsModalVisible(false);
+  };
+
   return (
     <div>
-      <div class="modal">
-        <img src="img.JPG" alt="" />
-        <div class="close"></div>
-      </div>
-      <div class="container">
-        <div class="card">
-          <div class="header">
-            <div class="hamburger-menu">
-              <div class="center"></div>
-            </div>
-            <a href="#" class="mail">
-              <i class="far fa-envelope"></i>
+      {isModalVisible && (
+        <div className="modal show">
+          <img src="https://i.pinimg.com/736x/96/91/28/9691288a3fadba6a8e6173d4eea20488.jpg" alt="Profile" />
+          <div className="close" onClick={hide}></div>
+        </div>
+      )}
+      <div className="container">
+        <div className="card">
+          <div className="header">
+            <a href="#" className="mail">
+              <FaEnvelope />
             </a>
-            <div class="main">
-              <div class="image">
-                <div class="hover">
-                  <i class="fas fa-camera fa-2x"></i>
+            <div className="main">
+              <div
+                className={`image ${isHoverActive ? 'active' : ''}`}
+                style={{ backgroundImage: "url('https://i.pinimg.com/736x/96/91/28/9691288a3fadba6a8e6173d4eea20488.jpg')" }}
+                onClick={show}
+              >
+                <div className="hover">
+                  <FaCamera className="fa-2x" />
                 </div>
               </div>
-              <h3 class="name">Ali Ataie</h3>
-              <h3 class="sub-name">@itsaliataie</h3>
+              <h3 className="name">Ali Ataie</h3>
+              <h3 className="sub-name">@itsaliataie</h3>
             </div>
           </div>
-          <div class="content">
-            <div class="left">
-              <div class="about-container">
-                <h3 class="title">About</h3>
-                <p class="text">
-                  An aspiring Frontend Developer working with CSS
-                  / JavaScript / React
+          <div className="content">
+            <div className="left">
+              <div className="about-container">
+                <h3 className="title">About</h3>
+                <p className="text">
+                  An aspiring Frontend Developer working with CSS / JavaScript / React
                 </p>
               </div>
-              <div class="icons-container">
-                <a href="#" class="icon">
-                  <i class="fab fa-facebook"></i>
+              <div className="icons-container flex flex-row gap-2 justify-center">
+                <a href="#" className="icon">
+                  <FaFacebook />
                 </a>
-                <a href="#" class="icon">
-                  <i class="fab fa-instagram"></i>
+                <a href="#" className="icon">
+                  <FaInstagram />
                 </a>
-                <a href="#" class="icon">
-                  <i class="fab fa-google-plus-g"></i>
+                <a href="#" className="icon">
+                  <FaGooglePlusG />
                 </a>
-                <a href="#" class="icon">
-                  <i class="fab fa-twitter"></i>
+                <a href="#" className="icon">
+                  <FaTwitter />
                 </a>
               </div>
-              <div class="buttons-wrap">
-                <div class="follow-wrap">
-                  <a href="#" class="follow">Follow</a>
+              <div className="buttons-wrap">
+                <div className="follow-wrap">
+                  <a href="#" className="follow">Follow</a>
                 </div>
-                <div class="share-wrap">
-                  <a href="#" class="share">Share</a>
+                <div className="share-wrap">
+                  <a href="#" className="share">Share</a>
                 </div>
-              </div>
-            </div>
-            <div class="right">
-              <div>
-                <h3 class="number">91</h3>
-                <h3 class="number-title">Posts</h3>
-              </div>
-              <div>
-                <h3 class="number">2.4K</h3>
-                <h3 class="number-title">Following</h3>
-              </div>
-              <div>
-                <h3 class="number">4.7K</h3>
-                <h3 class="number-title">Followers</h3>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProfileCard
+export default ProfileCard;
